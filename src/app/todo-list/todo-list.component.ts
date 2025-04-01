cf4c8e06309a7316c4440cfea370ff62eddebdd9
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-01 17:15:05 +0530

    Update todo-list.component.ts

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent {
-  tasks = [
-    { name: 'Learn Angular', completed: false },
-    { name: 'Write Blog Post', completed: false },
-    { name: 'Create a Portfolio', completed: false }
-  ];
+  tasks = [];
 }
