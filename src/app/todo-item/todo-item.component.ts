bc2578055592e9aa723e54bc0cacf522596bb8d9
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-01 17:15:56 +0530

    Update todo-item.component.ts

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -6,9 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent {
-  @Input() task!: { name: string, completed: boolean };
-
   toggleComplete() {
-    this.task.completed = !this.task.completed;
   }
 }
